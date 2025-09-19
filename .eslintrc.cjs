96fb5e760935b600c2d52e0d8cef2c1e4109e090
enzo.barbon <enzobl2000@gmail.com>
2025-09-19 22:56:59 +0200

    chore: init Expo (tabs, TS), add web/PWA meta, ESLint/Prettier, EAS, README

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+	root: true,
+	env: {
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		"@react-native",
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
+		"prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: { jsx: true },
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: undefined,
+	},
+	plugins: ["react", "react-native", "react-hooks", "@typescript-eslint", "import"],
+	rules: {
+		"react/react-in-jsx-scope": "off",
+		"import/order": [
+			"warn",
+			{
+				"newlines-between": "always",
+				"alphabetize": { order: "asc", caseInsensitive: true },
+			},
+		],
+	},
+	settings: {
+		react: { version: "detect" },
+		"import/resolver": {
+			typescript: {},
+			node: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+		},
+	},
+};
+
+
